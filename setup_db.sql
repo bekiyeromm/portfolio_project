033f581a7e475c14c67a807d5ba85f4071fd7f10
bekiyerom <57282706+bekiyeromm@users.noreply.github.com>
2023-06-21 15:30:58 +0300

    Update setup_db.sql

--- setup_db.sql
+++ setup_db.sql
@@ -1,5 +1,1 @@
-CREATE DATABASE IF NOT EXISTS pims_db;
-CREATE USER IF NOT EXISTS 'pims_user'@'localhost' IDENTIFIED BY 'pims_user_pwd';
-GRANT ALL PRIVILEGES ON `pims_db`.* TO 'pims_user'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'pims_user'@'localhost';
-FLUSH PRIVILEGES;
+
